4ab9fec7c30b67634d86044229d77f250199821f
Nikhil Kasukurthi <nikhil.kasukurthi@gmail.com>
2021-02-25 16:41:24 +0530

    Change folder structure and add flexibility to run rmf or npz

--- run_prism.py
+++ run_prism.py
@@ -5,20 +5,20 @@ from omegaconf import OmegaConf
 
 from generate_input.generate_distance_vectors import run as generate_dist_vectors
 from generate_input.get_model_coordinates import get_coordinates
-from train import start_training
+from src.train import start_training
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Train PrISM on given ensemble of integrative structure models.')
 
     parser.add_argument('--input',
-                        help="The path to rmf3 or files",
+                        help="The path to input files or folder in case of rmf3",
                         required=True)
     parser.add_argument('--output_dir',
                         help="Path to save the model_cooridnate files",
                         required=True)
     parser.add_argument('--type',
                         default='rmf',
-                        help="rmf or pdb input.")
+                        help="rmf, pdb or npz input.")
     parser.add_argument('--resolution',
                         help="The resolution to sample the beads",
                         default=1,
@@ -38,21 +38,27 @@ if __name__ == '__main__':
     # Get model coordinates.
     output_base_path = args.output_dir
     output_path = os.path.join(output_base_path, 'models_coordinate_npz')
-    if not os.path.exists(output_path):
-        os.makedirs(output_path)
-    resolution = int(args.resolution)
+    os.makedirs(output_path, exist_ok=True)
+    type = args.type
 
-    print("Getting the cooridnates from NPZ files with following parameters")
-    print("Input: {}, Output: {}".format(args.input, output_base_path))
-    print("Resolution: {}, Molecule: {}".format(resolution, args.molecule))
-    out_path = get_coordinates(args.input, output_base_path, output_path, resolution, args.molecule)
+    npz_path = None
+    if type == 'rmf':
+        resolution = int(args.resolution)
+        print("Getting the cooridnates from NPZ files with following parameters")
+        print("Input: {}, Output: {}".format(args.input, output_base_path))
+        print("Resolution: {}, Molecule: {}".format(resolution, args.molecule))
+        npz_path = get_coordinates(args.input, output_base_path, output_path, resolution, args.molecule)
+        type = 'npz'
 
     # Generate bead_wise distance.
-    print("Generating bead-wise distances")
-    generate_dist_vectors(out_path, args.output_dir)
+    if type == 'npz':
+        if not npz_path:
+            npz_path = args.input
+        print("Generating bead-wise distances")
+        generate_dist_vectors(npz_path, args.output_dir)
 
     conf = OmegaConf.load(args.config)
-    conf['dataset']['input_dir'] = out_path
+    conf['dataset']['input_dir'] = npz_path
     conf['dataset']['output_dir'] = args.output_dir
     conf['use_gpu'] = True if args.gpu == 1 else False
     print("Starting training")
---
+++ src/model_builder.py
@@ -0,0 +1,127 @@
+import numpy as np
+import pytorch_lightning as pl
+import torch
+import torch.utils.data
+from torch import optim
+from torch.nn import functional as F
+
+from model.ae_1d import AE1D
+from distance_vectors_dataset import DistanceVectorsDataset
+
+
+class ModelBuilder(pl.LightningModule):
+    """
+    Class that stores all the models, loss and executes training loop.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(ModelBuilder, self).__init__()
+        self.config = kwargs.pop('config')
+        self.loss = self.get_loss()
+        self.model = self.get_model()
+
+    def get_model(self):
+        """
+        Provides the AE architecture from config.
+        :return: Torch object
+        """
+        self.prepare_data()
+        self.config.model_params.input_size = self.dataset.input_size
+
+        model = AE1D(**self.config.model_params)
+        model = model.double()
+        return model
+
+    def get_loss(self):
+        def mse(**kwargs):
+            x = kwargs['x']
+            recon_x = kwargs['recon_x']
+            MSE = F.mse_loss(recon_x, x, reduction='sum')
+            loss = {'loss': MSE}
+
+            return loss
+
+        return mse
+
+    def prepare_data(self):
+        self.dataset = DistanceVectorsDataset(
+            self.config.dataset.input_dir,
+            apply_sigmoid_input=True,
+            normalize=True
+        )
+
+        n = len(self.dataset)  # how many total elements you have
+        val_len = int(np.ceil(n * 0.20))  # number of test/val elements
+        train_len = n - val_len
+
+        self.train_dataset, self.val_dataset = torch.utils.data.random_split(self.dataset, [train_len, val_len])
+
+    def train_dataloader(self):
+        train_loader = torch.utils.data.DataLoader(self.train_dataset,
+                                                   batch_size=self.config['batch_size'],
+                                                   shuffle=True,
+                                                   pin_memory=True,
+                                                   num_workers=16)
+        return train_loader
+
+    def val_dataloader(self):
+        val_loader = torch.utils.data.DataLoader(self.val_dataset,
+                                                 batch_size=self.config['batch_size'],
+                                                 shuffle=False,
+                                                 pin_memory=True,
+                                                 num_workers=16)
+        return val_loader
+
+    def configure_optimizers(self):
+        optimizer = optim.Adam(self.parameters(), lr=self.config.get("lr", 1e-3), weight_decay=0.5)
+        return optimizer
+
+    def training_step(self, batch, batch_index):
+        data = batch
+        output = self.model.forward(data)
+        loss = self.loss(**output)
+        self.log('train_loss', loss, on_step=True, prog_bar=True)
+        return loss
+
+    def validation_step(self, batch, batch_index):
+        data = batch
+        output = self.model.forward(data)
+        loss = self.loss(**output)
+        self.log('val_loss', loss, on_step=True, prog_bar=True)
+        return loss
+
+    def forward(self, *args, **kwargs):
+        return self.model.forward(*args, **kwargs)
+
+    def training_epoch_end(self, outputs):
+        keys = list(outputs[0].keys())
+        loss_dict = {}
+        for key in keys:
+            loss_dict.update({'train_' + key: torch.stack([x[key] for x in outputs]).mean()})
+
+        self.log('train_loss', loss_dict.get('train_loss'), on_epoch=True, prog_bar=True)
+
+    def validation_epoch_end(self, outputs):
+        keys = list(outputs[0].keys())
+        loss_dict = {}
+        for key in keys:
+            loss_dict.update({'val_' + key: torch.stack([x[key] for x in outputs]).mean()})
+
+        self.log('train_loss', loss_dict.get('val_loss'), on_epoch=True, prog_bar=True)
+
+    def on_fit_end(self):
+        random_input = torch.ones((1, self.dataset.input_size)).double()
+        beadwise_precision = self.forward(random_input)['recon_x'].detach().cpu().numpy()
+
+        def min_max_scaler(input_arr):
+            return (input_arr - np.min(input_arr)) / (np.max(input_arr) - np.min(input_arr))
+
+        scaled_precision = min_max_scaler(beadwise_precision)
+        print("Saving model in path: {}".format(self.save_path))
+
+        import os
+        with open(os.path.join(self.save_path, "precision.txt"), 'w') as f:
+            for i in scaled_precision[0]:
+                f.write(str(i) + " \n")
+
+        super(ModelBuilder, self).on_fit_end()
