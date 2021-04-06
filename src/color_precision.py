ae94a36e4568ca47020f7c40a9e0bd8cada40a9a
Shruthi Viswanath <shruthi.viswanath@gmail.com>
2021-04-06 17:09:57 +0530

    supporting PDB

--- src/color_precision.py
+++ src/color_precision.py
@@ -5,72 +5,100 @@ import IMP
 import RMF
 import IMP.rmf
 
-__doc__ = "Color the beads of the cluster representative model based on the precision."
+__doc__ = "Get an RMF where beads of the cluster representative model are colored \
+based on their precision as reported by PRISM."
 
 ###########################################################
 
 def parse_args():
     parser = ArgumentParser(
-            description="Color the beads of a cluster center model based on the bead precision as output from PRISM")
-    parser.add_argument('--path', '-p', dest="path",
-            help='path to the good-scoring models', default="./")
-    parser.add_argument('--clusternum', '-c', dest="cluster_number",type=int,
-            help='cluster index of model to be colored; required argument', default=0)
+            description="Color the regions of a representative model based on the precision as output from PRISM")
+    parser.add_argument('--input', '-i', dest="input",
+            help='representative model in RMF or PDB format', default="cluster.0/cluster_center_model.rmf3",required=True)
     parser.add_argument('--subunit', '-su', dest="subunit",
-            help='calculate RMSD/sampling and cluster precision/densities '
-                 'etc over this subunit only', default=None)
+            help='annotate variation in precision over this subunit only', default=None)
     parser.add_argument('--resolution', '-r', dest="resolution", type=int,
-            help='bead size (residues per bead) for RMSD and precision calculations; '
-                 'use biggest bead size for faster calculations', default=1)
+            help='bead size (residues per bead) for annotating precision', default=1)
     parser.add_argument('--precision_file','-pf',dest="precision_file",required=True,type=str,
-            help='location of file containing precision value per bead; one precision per line; required argument')
+            help='location of output from the autoencoder; one precision per line; required argument')
+    parser.add_argument('--output', '-o', dest="output",
+            help='precision-colored model in RMF format. Visualize using Chimera.', default="precision_colored_cluster_center_model.rmf3")
 
     return parser.parse_args()
 
-def get_bead_name(p):
+def get_bead_name(p, input_type):
     ''' Input: particle
     Output: bead name in the format moleculename_copynumber_startresidue_endresidue
     '''
 
-    mol_name = IMP.atom.get_molecule_name(IMP.atom.Hierarchy(p))
+    if input_type=="rmf":
 
-    copy_number=IMP.atom.get_copy_index(IMP.atom.Hierarchy(p))
+        mol_name = IMP.atom.get_molecule_name(IMP.atom.Hierarchy(p))
 
-    if IMP.atom.Fragment.get_is_setup(p):
-        residues_in_bead = IMP.atom.Fragment(p).get_residue_indexes()
+        copy_number=IMP.atom.get_copy_index(IMP.atom.Hierarchy(p))
 
-        bead_name = mol_name+":"+str(copy_number)+":"+str(min(residues_in_bead))+"-"+str(max(residues_in_bead))
+        if IMP.atom.Fragment.get_is_setup(p):
+            residues_in_bead = IMP.atom.Fragment(p).get_residue_indexes()
 
-    else:
-        residue_in_bead = str(IMP.atom.Residue(p).get_index())
+            bead_name = mol_name+":"+str(copy_number)+":"+str(min(residues_in_bead))+"-"+str(max(residues_in_bead))
 
-        bead_name = mol_name+":"+str(copy_number)+":"+residue_in_bead+"-"+residue_in_bead
+        else:
+            residue_in_bead = str(IMP.atom.Residue(p).get_index())
 
-    return bead_name
+            bead_name = mol_name+":"+str(copy_number)+":"+residue_in_bead+"-"+residue_in_bead
+
+        return bead_name
+
+    elif input_type =="pdb":
+
+        mol_name = IMP.atom.get_molecule_name(IMP.atom.Hierarchy(p))
+
+        residue_number = IMP.atom.Residue(IMP.atom.Hierarchy(p).get_parent()).get_index()
+
+        bead_name = mol_name+":"+str(residue_number)
+
+        return bead_name
 
 def main():
     args = parse_args()
 
     # Open representative model
-    cluster_centroid = os.path.join(args.path,"cluster.%s/cluster_center_model.rmf3" %str(args.cluster_number))
 
-    if not os.path.exists(cluster_centroid):
-        print("Cluster centroid file not found for cluster %d.",args.cluster_number)
+    if not os.path.exists(args.input):
+        print("Cluster representative file not found %s",args.input)
         exit(1)
 
-    rmf_fh = RMF.open_rmf_file_read_only(cluster_centroid)
-
-    # Build hierarchy from the RMF file
+    # create model and hierarchy for input file
     m = IMP.Model()
-    h = IMP.rmf.create_hierarchies(rmf_fh, m)[0]
-    IMP.rmf.load_frame(rmf_fh, 0)
-    m.update()
+    if args.input.lower().endswith("rmf") or args.input.lower().endswith("rmf3"):
 
-    # Select particles to color by precision
-    if args.subunit:
-        s0 = IMP.atom.Selection(h, resolution=args.resolution, molecule=args.subunit)
+        input_type = "rmf"
+
+        rmf_fh = RMF.open_rmf_file_read_only(args.input)
+
+        # Build hierarchy from the RMF file
+        h = IMP.rmf.create_hierarchies(rmf_fh, m)[0]
+        IMP.rmf.load_frame(rmf_fh, 0)
+
+        m.update()
+
+        # Select particles to color by precision
+        if args.subunit:
+            s0 = IMP.atom.Selection(h, resolution=args.resolution, molecule=args.subunit)
+
+        else:
+            s0 = IMP.atom.Selection(h, resolution=args.resolution)
+
+    elif args.input.lower().endswith("pdb"):
+        input_type = "pdb"
+
+        h = IMP.atom.read_pdb(args.input, m, IMP.atom.CAlphaPDBSelector())
+
+        s0 = IMP.atom.Selection(h)
+
     else:
-        s0 = IMP.atom.Selection(h, resolution=args.resolution)
+        print("Input file is not in RMF or PDB format.")
+        exit(1)
 
     particles = s0.get_selected_particles()
 
@@ -92,14 +120,14 @@ def main():
     h_root = IMP.atom.Hierarchy.setup_particle(m_new,p_root)
 
     # Simultaneously output precisions with bead names to a text file for user to see
-    precisions_out_file = open(os.path.join(args.path,"cluster.%s/bead_precisions.txt"  %str(args.cluster_number)),'w')
+    precisions_out_file = open('bead_precisions.txt','w')
 
     for i,leaf in enumerate(particles):
 
         #ASSUMPTION Assuming single state models for now
         # One can make this multi-state by adding state name in the bead name
 
-        bead_name = get_bead_name(leaf)
+        bead_name = get_bead_name(leaf,input_type)
 
         # Create a new particle
         p_new = m_new.add_particle(bead_name)
@@ -121,7 +149,7 @@ def main():
     precisions_out_file.close()
 
     # Now create a new RMF file with the new model
-    rmf_new = RMF.create_rmf_file(os.path.join(args.path,"cluster.%s/precision_colored_cluster_center_model.rmf3" %str(args.cluster_number)))
+    rmf_new = RMF.create_rmf_file(args.output)
 
     IMP.rmf.add_hierarchy(rmf_new,h_root)
     IMP.rmf.save_frame(rmf_new)
