600524c852b34f553d7a3e30685b1d332ee396cc
Nikhil Kasukurthi <nikhil.kasukurthi@gmail.com>
2021-05-13 15:44:58 +0530

    Update precision_file to be consistent with Readme.Md

--- src/model_builder.py
+++ src/model_builder.py
@@ -109,7 +109,7 @@ class ModelBuilder(pl.LightningModule):
             return (input_arr - np.min(input_arr)) / (np.max(input_arr) - np.min(input_arr))
 
         scaled_precision = min_max_scaler(beadwise_precision)
-        print("Saving color_precision file in path: {}".format(self.color_precision_save_path))
+        print("Saving precision_file in path: {}".format(self.color_precision_save_path))
 
         import os
         with open(os.path.join(self.save_path, "precision.txt"), 'w') as f:
