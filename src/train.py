f21a4c7bec1196303ed26c0d897bf0d98a3dc90c
Nikhil Kasukurthi <nikhil.kasukurthi@gmail.com>
2021-05-18 13:07:27 +0530

    Add unit tests for input generation and model_training

--- src/train.py
+++ src/train.py
@@ -6,7 +6,7 @@ from omegaconf import OmegaConf
 from pytorch_lightning import Trainer
 from pytorch_lightning.loggers import TestTubeLogger
 
-from model_builder import ModelBuilder
+from .model_builder import ModelBuilder
 
 
 def start_training(config, gpu):
@@ -44,10 +44,9 @@ def start_training(config, gpu):
         logger=tt_logger,
         max_epochs=conf.max_epochs,
         num_sanity_val_steps=5,
-        gpus=[gpu]
+        gpus=gpu
     )
     runner.fit(m)
-    m.summarize()
 
 
 if __name__ == '__main__':
