08ac8f1a00aba1f06a7ddd93aad0b7611e991935
Kenobi98 <majilakartik123@gmail.com>
2023-06-05 09:53:54 +0530

    Corrected calling parse_custom_ranges

--- src/rmf_parser.py
+++ src/rmf_parser.py
@@ -92,7 +92,7 @@ def get_attributes(str_file, input_type, resolution, subunit, selection):
 
 def parse_all_rmfs(path, resolution, subunit, selection):
     # Read the selected particles.
-    selection = IMP.sampcon.rmsd_calculation.parse_custom_ranges( selection )
+    selection = IMP.sampcon.precision_rmsd.parse_custom_ranges( selection )
     files_path = glob.glob(os.path.join(path, "*.rmf3" ))
     print('Files Detected: ', files_path)
     with Pool(16) as p:
